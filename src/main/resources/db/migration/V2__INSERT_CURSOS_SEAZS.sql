e50e3fb374041bb480d0a44419115865f664c18a
Gustavo Santos Valverde <gustavo.valverde5@gmail.com>
2019-07-28 15:28:09 -0300

    Atualiza migrations para inserir cursos e usuários.

---
+++ src/main/resources/db/migration/V2__INSERT_CURSOS_SEAZS.sql
@@ -0,0 +1,4 @@
+-- Insere Cursos
+INSERT into seazs.tb_curso (modalidade_curso, nome_curso, qtd_semestre) VALUES (1, 'Análise e Desenvolvimento de Sistemas', 6);
+INSERT into seazs.tb_curso (modalidade_curso, nome_curso, qtd_semestre) VALUES (1, 'Logística', 6);
+INSERT into seazs.tb_curso (modalidade_curso, nome_curso, qtd_semestre) VALUES (3, 'Gestão Empresarial', 6);
